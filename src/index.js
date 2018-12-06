eb36e972eafc53bd131d2cfd8ac1079c28d8a461
Tucker <trblackw@ncsu.edu>
2018-12-06 17:54:13 -0500

    added route for specific citation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { render } from "react-dom";
 import Landing from "./components/Landing";
+import CitationForm from "./components/CitationForm";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={Landing} />
+      <Route path="/:type/:style" component={CitationForm} />
     </Switch>
   </Router>
 );
