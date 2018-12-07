1a1a2a0058db2563bbad7ccd15d16b85e5bb8686
Tucker <trblackw@ncsu.edu>
2018-12-07 10:45:17 -0500

    created component for Book citations

---
+++ src/components/Book.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { FlexForm } from "./elements/form_elements";
+import { useInputValue } from "./hooks/useInputValue";
+
+const Book = () => {
+  const title = useInputValue("");
+  const publisher = useInputValue("");
+  const location = useInputValue("");
+  const year = useInputValue("");
+  return (
+    <FlexForm>
+      <label htmlFor="title">Title</label>
+      <input type="text" name="title" {...title} />
+      <label htmlFor="publisher">Publisher</label>
+      <input type="text" name="publisher" {...publisher} />
+      <label htmlFor="location">Location</label>
+      <input type="text" name="location" {...location} />
+      <label htmlFor="year">Year</label>
+      <input type="text" name="year" {...year} />
+    </FlexForm>
+  );
+};
+
+export default Book;
