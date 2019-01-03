3bd88d3397039fe39058717c67941841f7bde6b5
Tucker <trblackw@ncsu.edu>
2019-01-03 13:39:07 -0500

    reformatted Book.js state management

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { FlexForm } from "./elements/index";
 import { useInputValue } from "./hooks/useInputValue";
 
+const initialFormState = {
+  title: "",
+  publisher: "",
+  location: "",
+  year: ""
+};
+
 const Book = () => {
-  const title = useInputValue("");
-  const publisher = useInputValue("");
-  const location = useInputValue("");
-  const year = useInputValue("");
+  const [form, setForm] = useState(initialFormState);
+  const [book, setBook] = useState(null);
+
+  const handleInput = e => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setBook(form);
+    setForm(initialFormState);
+  };
   return (
-    <>
+    <form onSubmit={e => handleSubmit(e)}>
       <label htmlFor="title">Title</label>
-      <input type="text" name="title" {...title} />
+      <input type="text" name="title" onChange={e => handleInput(e)} />
       <label htmlFor="publisher">Publisher</label>
-      <input type="text" name="publisher" {...publisher} />
+      <input type="text" name="publisher" onChange={e => handleInput(e)} />
       <label htmlFor="location">Location</label>
-      <input type="text" name="location" {...location} />
+      <input type="text" name="location" onChange={e => handleInput(e)} />
       <label htmlFor="year">Year</label>
-      <input type="text" name="year" {...year} />
-    </>
+      <input type="text" name="year" onChange={e => handleInput(e)} />
+    </form>
   );
 };
 
